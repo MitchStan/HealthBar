d074fda1838704914497f258255721d59689d393
MitchStan <31759531+MitchStan@users.noreply.github.com>
2020-03-03 19:36:01 -0800

    Practice

--- HealthBar/Assets/HealthBar.cs
+++ HealthBar/Assets/HealthBar.cs
@@ -6,23 +6,28 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
 	public Slider slider;
+	public Gradient gradient;
+	public Image fill;
 
 
 	public void SetMaxHealth (int health)
 	{
 		slider.maxValue = health;
 		slider.value = health;
+		fill.color = gradient.Evaluate(1f);
 	}
 
 	public void SetHealth (int health)
 	{
 		slider.value = health;
+
+		fill.color = gradient.Evaluate(slider.normalizedValue);
 	}
 }
 
 
 /*
-public class HealthBar : MonoBehaviour
+public class HealthBar :   MonoBehaviour
 {
 
 	public Slider slider;
